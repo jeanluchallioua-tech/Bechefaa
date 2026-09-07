f6b21e82ad04dc244cbf0d66327a987d26664c7a
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-07 15:12:49 +0200

    Active ergonomie tactile caisse

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -9,9 +9,11 @@ from clean_caisse.history_modifier import register_history_modifier
 from clean_caisse.customer_phase1 import register_customer_phase1
 from clean_caisse.delivery_zones import register_delivery_zones
 from clean_caisse.pos_quick_add import register_pos_quick_add
+from clean_caisse.pos_touch_layout import register_pos_touch_layout
 
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
 register_history_modifier(app, db, ensure_order_schema, order_payload)
 register_customer_phase1(app, db, ensure_order_schema)
 register_delivery_zones(app, db)
 register_pos_quick_add(app)
+register_pos_touch_layout(app)
