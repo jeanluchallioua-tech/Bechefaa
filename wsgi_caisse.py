bc2e74faa1abe17ac8cbb19c0bb559d4495fbc95
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-07 20:32:00 +0200

    Register Phase 2.4 read-only category diagnostic

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -21,6 +21,7 @@ from clean_caisse.options_diagnostic_phase2 import register_options_diagnostic_p
 from clean_caisse.option_price_admin_phase23 import register_option_price_admin_phase23
 from clean_caisse.option_add_test_phase23 import register_option_add_test_phase23
 from clean_caisse.option_rules_test_phase23 import register_option_rules_test_phase23
+from clean_caisse.category_diagnostic_phase24 import register_category_diagnostic_phase24
 
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
 register_history_modifier(app, db, ensure_order_schema, order_payload)
@@ -41,3 +42,4 @@ register_options_diagnostic_phase2(app, db)
 register_option_price_admin_phase23(app, db)
 register_option_add_test_phase23(app, db)
 register_option_rules_test_phase23(app, db)
+register_category_diagnostic_phase24(app, db)
