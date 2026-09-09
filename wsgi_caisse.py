391b4e1931e579512f9373e56e254d2156c91bd3
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-09 10:30:06 +0200

    Phase 4.2: brancher la simplification du mode de service

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -50,6 +50,7 @@ from clean_caisse.z_lock_phase37 import register_z_lock_phase37
 from clean_caisse.pos_quantity_phase38 import register_pos_quantity_phase38
 from clean_caisse.payment_core_phase41 import register_payment_core_phase41
 from clean_caisse.payment_history_ui_phase41 import register_payment_history_ui_phase41
+from clean_caisse.service_mode_ui_phase42 import register_service_mode_ui_phase42
 
 register_catalog_summary_fast_phase32(app, load_catalog)
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
@@ -97,3 +98,4 @@ register_z_lock_phase37(app, db, ensure_order_schema)
 register_pos_quantity_phase38(app)
 register_payment_core_phase41(app, db, ensure_order_schema)
 register_payment_history_ui_phase41(app)
+register_service_mode_ui_phase42(app)
