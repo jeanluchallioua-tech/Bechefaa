ada2054c8bde67adbf272e2927231c5cd507531b
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-09 16:55:29 +0200

    Phase 4.4 register cash to Z link

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -76,6 +76,7 @@ from clean_caisse.cash_float_phase44 import register_cash_float_phase44
 from clean_caisse.cash_count_test_phase44 import register_cash_count_test_phase44
 from clean_caisse.cash_count_phase44 import register_cash_count_phase44
 from clean_caisse.z_cash_link_test_phase44 import register_z_cash_link_test_phase44
+from clean_caisse.z_cash_link_phase44 import register_z_cash_link_phase44
 
 register_catalog_summary_fast_phase32(app, load_catalog)
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
@@ -149,3 +150,4 @@ register_cash_float_phase44(app, db)
 register_cash_count_test_phase44(app, db)
 register_cash_count_phase44(app, db)
 register_z_cash_link_test_phase44(app, db)
+register_z_cash_link_phase44(app, db)
