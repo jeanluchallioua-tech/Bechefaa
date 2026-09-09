ebe5ad2dc268be49462eb4fb629a1f2e5fa5344f
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-09 16:34:40 +0200

    Phase 4.4 register isolated cash float preview

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -71,6 +71,7 @@ from clean_caisse.refund_action_ui_phase44 import register_refund_action_ui_phas
 from clean_caisse.payment_refund_guard_phase44 import register_payment_refund_guard_phase44
 from clean_caisse.payment_guard_test_phase44 import register_payment_guard_test_phase44
 from clean_caisse.transaction_journal_test_phase44 import register_transaction_journal_test_phase44
+from clean_caisse.cash_float_test_phase44 import register_cash_float_test_phase44
 
 register_catalog_summary_fast_phase32(app, load_catalog)
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
@@ -139,3 +140,4 @@ register_refund_action_ui_phase44(app)
 register_payment_refund_guard_phase44(app, db)
 register_payment_guard_test_phase44(app)
 register_transaction_journal_test_phase44(app, db)
+register_cash_float_test_phase44(app, db)
