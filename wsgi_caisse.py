587fa5b66707e062aa0593f54ee56de32334f335
jeanluchallioua-tech <jeanluchallioua@gmail.com>
2026-09-24 18:00:44 +0200

    Branche l'ecran de remise a zero avant mise en service

--- wsgi_caisse.py
+++ wsgi_caisse.py
@@ -125,6 +125,7 @@ from clean_caisse.site_edenred_checkout_isolated_phase6 import register_site_ede
 from clean_caisse.site_edenred_prod_checkout_isolated_phase6 import register_site_edenred_prod_checkout_isolated_phase6
 from clean_caisse.edenred_uat_cleanup_phase6 import register_edenred_uat_cleanup_phase6
 from clean_caisse.access_pin_phase6 import register_access_pin_phase6
+from clean_caisse.go_live_reset_phase6 import register_go_live_reset_phase6
 
 register_catalog_summary_fast_phase32(app, load_catalog)
 register_kitchen_checklist(app, db, ensure_order_schema, order_payload)
@@ -247,3 +248,4 @@ register_site_edenred_checkout_isolated_phase6(app)
 register_site_edenred_prod_checkout_isolated_phase6(app)
 register_edenred_uat_cleanup_phase6(app, db, ensure_order_schema)
 register_access_pin_phase6(app)
+register_go_live_reset_phase6(app, db)
